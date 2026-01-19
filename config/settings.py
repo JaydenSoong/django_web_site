cf460c99ee979de7a3e8a96547f75147c93ec7a1
JaydenSoong <jiandongsong@126.com>
2026-01-19 15:27:06 +0800

    渲染静态文件

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 指定 templates 目录
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# 配置静态文件目录
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import hello
+from mysite.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello),
+    # 注意，这里把 route 参数设置为空字符串，这样，当访问 / 时，就会调用 index 函数
+    path('', index),
 ]
